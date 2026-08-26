b35ef9345f307ddaa830785ea12bccaa3860e1f1
Karmoush <karmaminecraft00@gmail.com>
2026-08-26 16:52:21 +0100

    Initial NixOS configuration

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Karmoush's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.karmoush = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
