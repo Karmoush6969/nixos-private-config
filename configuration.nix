92b662b3909f493370d5d50141c5d4320766e395
Karmoush <karmaminecraft00@gmail.com>
2026-08-27 10:56:16 +0100

    Update configuration

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 2;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.timeout = 0;  
 
 
