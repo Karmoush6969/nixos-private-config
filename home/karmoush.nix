703d459e9701c1e25ecef12f7978b4500f2da8b4
Karmoush <karmaminecraft00@gmail.com>
2026-08-30 23:03:52 +0100

    Fix Home Manager config

--- home/karmoush.nix
+++ home/karmoush.nix
@@ -1,50 +1,103 @@
 { config, pkgs, ... }:
 
 {
-home.username = "karmoush";
-home.homeDirectory = "/home/karmoush";
+  home.username = "karmoush";
+  home.homeDirectory = "/home/karmoush";
 
-home.stateVersion = "26.05";
+  home.stateVersion = "26.05";
 
-home.file = {
-".scripts".source = ../scripts;
+  home.file = {
+    ".scripts" = {
+      source = ../scripts;
+      force = true;
+    };
 
+    ".pki/nssdb" = {
+      source = ../.pki/nssdb;
+      force = true;
+    };
 
-".config/btop".source = ../.config/btop;
-".config/cava".source = ../.config/cava;
-".config/fastfetch".source = ../.config/fastfetch;
-".config/gtk-3.0".source = ../.config/gtk-3.0;
-".config/gtk-4.0".source = ../.config/gtk-4.0;
-".config/hypr".source = ../.config/hypr;
-".config/kitty".source = ../.config/kitty;
-".config/noctalia".source = ../.config/noctalia;
+    ".config/btop" = {
+      source = ../.config/btop;
+      force = true;
+    };
 
-".config/kdeglobals".source =
-  ../.config/kde/kdeglobals;
+    ".config/cava" = {
+      source = ../.config/cava;
+      force = true;
+    };
 
-".config/kwinrc".source =
-  ../.config/kde/kwinrc;
+    ".config/fastfetch" = {
+      source = ../.config/fastfetch;
+      force = true;
+    };
 
-".config/plasmarc".source =
-  ../.config/kde/plasmarc;
+    ".config/gtk-3.0" = {
+      source = ../.config/gtk-3.0;
+      force = true;
+    };
 
+    ".config/gtk-4.0" = {
+      source = ../.config/gtk-4.0;
+      force = true;
+    };
 
+    ".config/hypr" = {
+      source = ../.config/hypr;
+      force = true;
+    };
 
-".config/plasma-org.kde.plasma.desktop-appletsrc" = {
-  source = ../.config/kde/plasma-org.kde.plasma.desktop-appletsrc;
-  force = true;
-};
+    ".config/kitty" = {
+      source = ../.config/kitty;
+      force = true;
+    };
 
-".config/GIMP/3.0".source = ../.config/GIMP/3.0;
+    ".config/noctalia" = {
+      source = ../.config/noctalia;
+      force = true;
+    };
 
-".local/share/applications/org.gimp.GIMP.desktop".source =
-  ../.local/share/applications/org.gimp.GIMP.desktop;
+    ".config/kdeglobals" = {
+      source = ../.config/kde/kdeglobals;
+      force = true;
+    };
 
-".local/share/icons/hicolor/photogimp.png".source =
-  ../.local/share/icons/hicolor/photogimp.png;
+    ".config/kwinrc" = {
+      source = ../.config/kde/kwinrc;
+      force = true;
+    };
 
+    ".config/plasmarc" = {
+      source = ../.config/kde/plasmarc;
+      force = true;
+    };
 
-};
+    ".config/plasma-org.kde.plasma.desktop-appletsrc" = {
+      source = ../.config/kde/plasma-org.kde.plasma.desktop-appletsrc;
+      force = true;
+    };
 
-programs.home-manager.enable = true;
+    ".config/GIMP/3.0" = {
+      source = ../.config/GIMP/3.0;
+      force = true;
+    };
+
+    ".local/share/applications/org.gimp.GIMP.desktop" = {
+      source = ../.local/share/applications/org.gimp.GIMP.desktop;
+      force = true;
+    };
+
+    ".local/share/icons/hicolor/photogimp.png" = {
+      source = ../.local/share/icons/hicolor/photogimp.png;
+      force = true;
+    };
+  };
+
+  programs.home-manager.enable = true;
 }
+:::
+
+Then run:
+
+```bash
+sudo nixos-rebuild switch
